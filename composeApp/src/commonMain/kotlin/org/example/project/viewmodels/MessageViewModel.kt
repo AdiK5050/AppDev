72d6a120c770a67bbc5b2bfc4e5cea1774673e54
SkashEU <48493983+SkashEU@users.noreply.github.com>
2025-11-17 16:29:24 +0100

    fix: message list not displaying

--- composeApp/src/commonMain/kotlin/org/example/project/viewmodels/MessageViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/viewmodels/MessageViewModel.kt
@@ -2,13 +2,16 @@ package org.example.project.viewmodels
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.forEach
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import org.example.project.storage.AppDatabase
 import org.example.project.storage.MessageEntity
@@ -17,14 +20,20 @@ import org.example.project.storage.UserSession
 class MessageViewModel(appDatabase: AppDatabase, val userSession: UserSession) : ViewModel() {
 
     val messageDao = appDatabase.getMessageDao()
-     val messageHistory = mutableStateOf(messageDao.getAllAsFlow())
-     val uidFrom = mutableStateOf(0)
-     val uidTo : MutableState<Int?> = mutableStateOf(null)
+    val messageHistory = messageDao.getAllAsFlow()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
+
+    val uidFrom = mutableIntStateOf(0)
+    val uidTo: MutableState<Int?> = mutableStateOf(null)
 
     fun addMessage(message: String) {
         viewModelScope.launch(Dispatchers.IO) {
-            messageDao.insert(uidTo.value,uidFrom.value,message)
-            if(uidTo.value == null) println("uidTo was null")
+            messageDao.insert(MessageEntity(uidTo = uidTo.value, uidFrom = uidFrom.intValue, message = message))
+            if (uidTo.value == null) println("uidTo was null")
             else println("uidTo was not null")
         }
     }
